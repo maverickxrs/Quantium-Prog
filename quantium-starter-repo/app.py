5994bdc4796b4a7e9a1f268f4a9ae7ea1d703b1b
Hamza <hamza.lbelghiti@gmail.com>
2022-07-23 01:20:36 +0100

    Improved Dash App

--- quantium-starter-repo/app.py
+++ quantium-starter-repo/app.py
@@ -1,40 +1,49 @@
 
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
+
 app = Dash(__name__)
+
+app.title = "Soul Foods Data Viz"
 
 df = pd.read_csv('./data/processedDF2')
 
-colors = {
-    'background': '#fff',
-    'text': '#000'
-}
-
 fig = px.line(df, x="date", y="sales")
 
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+app.layout = html.Div(style={'padding': 10}, children=[
     html.H1(
-        children='Pink Morsel Sales From October 2020 to Frebruary 2022',
+        children='Pink Morsel Sales Visualization',
         style={
             'textAlign': 'center',
-            'color': colors['text'],
             'font-family':'sans-serif'
         }
     ),
+    
+    html.Div([
+      html.Label('Choose the region : '),
+      html.Br(),
+      dcc.RadioItems([{'label': 'Noth', 'value': 'north'}, {'label': 'South', 'value': 'south'},
+      {'label': 'East', 'value': 'east'}, {'label': 'West', 'value': 'west'}], 'north', id='region', inline=True),
+    ], style={'display': 'flex', 'textAlign': 'center', 'font-family':'sans-serif', 'alignItems': 'center', 'justifyContent': 'center', 'margin': '10px'}),
 
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
+    dcc.Graph( 
+        id='salesGraph',
+        figure=fig,
     )
 ])
+
+@app.callback(
+    Output('salesGraph', 'figure'),
+    Input('region', 'value'))
+
+def update_graph(region):
+    dff = df[df['region'] == region]
+
+    fig = px.line(dff, x="date", y="sales", template = "ggplot2")
+
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
